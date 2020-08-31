f4646a960428a27630e84c0574ccbfb7b34d5b4d
Timothy Regan <tregan@pillartechnology.com>
2020-08-31 11:41:27 -0400

    linting

--- cota_tvier_adapter/__init__.py
+++ cota_tvier_adapter/__init__.py
@@ -1,3 +1,6 @@
+"""
+This is the controller.
+"""
 import csv
 import gzip
 import io
@@ -6,19 +9,22 @@ import re
 import tarfile
 import zipfile
 from functools import reduce
-from cota_tvier_adapter.record_converter import convert_row
 
 import requests
+import uvicorn
 from fastapi import FastAPI
+
+from cota_tvier_adapter.record_converter import convert_row
 
 app = FastAPI()
 
 search = re.compile(r"_obu_(.*?)\.")
 
-
 @app.get("/api/v1/tvier")
-async def do_tvier(url: str):
-    print("grabbing from url", url)
+async def tvier(url: str):
+    """
+    Downloads a gzip archive from the provided url and extracts data from the contained csv files
+    """
     response = requests.get(url)
     archive_bytes = io.BytesIO(response.content)
     return _process_tar_archive(archive_bytes)
@@ -27,7 +33,9 @@ async def do_tvier(url: str):
 def _process_tar_archive(tar_bytes):
     with tarfile.open(fileobj=tar_bytes) as tar_file:
         members = tar_file.getmembers()
-        return reduce(lambda rows, tar_info: _extract_gzips(rows, tar_info, tar_file), members, [])
+        return reduce(
+            lambda rows, tar_info: _extract_gzips(rows, tar_info, tar_file), members, []
+        )
 
 
 def _extract_gzips(rows, tar_info, tar_file):
@@ -41,13 +49,15 @@ def _process_gzip(gzip_file, source_device):
     with gzip.open(gzip_file, mode="rt") as csv_file:
         data = csv.DictReader(csv_file)
         converted_data = reduce(convert_row, data, [])
-        return list(map(lambda row: _add_source_device(row, source_device), converted_data))
+        return list(
+            map(lambda row: _add_source_device(row, source_device), converted_data)
+        )
+
 
 def _add_source_device(row, source_device):
     row.update({"sourceDevice": source_device})
     return row
 
 
-
 if __name__ == "__main__":
-    app
+    uvicorn.run("cota_tvier_adapter:app", host="127.0.0.1", port=5000, log_level="info")
--- cota_tvier_adapter/record_converter.py
+++ cota_tvier_adapter/record_converter.py
@@ -1,36 +1,45 @@
-from xmljson import parker
+"""
+Converts TVIER records to the standard SCOS CVE schema
+"""
 from xml.etree.ElementTree import fromstring
+
+from xmljson import parker
 
 event_map = {"receivedBSM": "BSM", "sentBSM": "BSM"}
 
+
 def convert_row(rows, row):
+    """
+    Maps the TVIER format to SCOS
+    """
     event_type = row["EventType"]
-    
+
     obj = {}
     obj["timestamp"] = row["Timestamp"]
     if event_type in event_map.keys():
         (message_body, event_type) = _convert_known_event(row)
     else:
         (message_body, event_type) = _convert_unknown_event(row)
-    
+
     obj["messageBody"] = message_body
     obj["messageType"] = event_type
 
     rows.append(obj)
     return rows
 
+
 def _convert_known_event(event):
     event_data = _convert_body(event["EventData"])
-    message_body = list(
-        event_data["message"]["MessageFrame"]["value"].values()
-    )[0]
+    message_body = list(event_data["message"]["MessageFrame"]["value"].values())[0]
     event_type = event_map[event["EventType"]]
     return (message_body, event_type)
+
 
 def _convert_unknown_event(event):
     message_body = _convert_body(event["EventData"])
     event_type = event["EventType"]
     return (message_body, event_type)
+
 
 def _convert_body(xml):
     return parker.data(fromstring(xml))
